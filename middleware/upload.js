c6d169b145b9db4518452170c361a0abb28af990
Megha <megha2045m@gmail.com>
2026-07-03 12:35:32 +0530

    Create uploads directory automatically

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,17 +1,9 @@
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
-
-// Create uploads directory if it doesn't exist
-const uploadDir = path.join(__dirname, "../uploads");
-
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);
+    cb(null, "uploads/");
   },
 
   filename: (req, file, cb) => {
